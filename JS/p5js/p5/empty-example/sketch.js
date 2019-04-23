166b07ca920467aebae74be0a2acfbb8bf2104d9
Mihael G <mihael147work@gmail.com>
2019-04-23 23:16:47 +0300

    Make same tests in p5.js

--- JS/p5js/p5/empty-example/sketch.js
+++ JS/p5js/p5/empty-example/sketch.js
@@ -1,14 +1,67 @@
 var hintImage, skyImage, stars = [];
 
+let epY = 0;
+let epX = 0;
+let epZ = 0;
+let size = 100;
+
+//let skyImage;
+let rhTex;;
+
 function setup() {
 
 
-createCanvas(800,500);
-noCursor();
-noStroke();
+createCanvas(windowWidth, windowHeight, WEBGL);
+//noCursor();
+//noStroke();
 
+
+//hintImage = loadImage("//bit.ly/hintImage");
+skyImage = loadImage("bgsky.jpg");
+rhTex = loadImage("bmstu.png");
+
+ 
 }
 
 function draw() {
-  // put drawing code here
+
+
+background(200,100,100);
+
+
+//image(skyImage,0,0)
+//ellipse(200, 200, 50, 50);
+
+//epZ = mouseY - 200;
+//epZ = map(mouseY, 0, -100, 0, height);
+epZ = map(mouseY, 100, height, 0, -500);
+epX = map(mouseX, 100, 1000, 0, -500);
+epY = map(mouseY, 100, 1000, 0, -500);
+
+//plane(50, 50);
+
+rotateX(-mouseX * 0.01);
+rotateY(-mouseY * 0.01);
+box(50, 50, 50, 3, 3);
+
+renderEggPlanet();
+
+
+console.log("draw function!");
+
+
+}
+
+
+
+function renderEggPlanet(){
+
+	texture(rhTex);
+	translate(epX,epY,epZ);
+	//rotateY(radians(mouseX/4));
+	rotateZ(radians(-mouseX/4));
+	rotateX(radians(-mouseY/4));
+	
+	sphere(size);
+
 }
